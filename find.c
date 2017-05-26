5513a5170a2872c24b71fe44c0a507b8562c41b4
Tufis Alex <dadya1996@gmail.com>
2017-05-26 04:02:35 +0300

    Update find.c

--- find.c
+++ find.c
@@ -1,3 +1,6 @@
+#define __USE_XOPEN
+#define _GNU_SOURCE
+
 #include <dirent.h>
 #include <getopt.h>
 #include <regex.h>
@@ -7,19 +10,18 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#ifndef _XOPEN_SOURCE
-#define _XOPEN_SOURCE 4
-#endif
 #include <time.h>
+#include <unistd.h>
 
 char s[1204];
+int nr = 0;
 
 int *User_Id = NULL;
 int *Group_Id = NULL;
 int *Hardlink_Max = NULL;
 int *Hardlink_Min = NULL;
 char *Iname = NULL;
+char *Exec = NULL;
 char *Permisiuni = NULL;
 float *Dim_Max = NULL;
 float *Dim_Min = NULL;
@@ -28,12 +30,58 @@ time_t *Timestamp_Max = NULL;
 time_t *Timestamp_Mdata_Max = NULL;
 time_t *Timestamp_Mdata_Min = NULL;
 
+float compare(char *size) {
+
+  int s = strlen(size);
+  if (s < 2)
+    return atol(size);
+  if (strcmp(size + (s - 2), "GB") == 0) {
+    size[s - 2] = 0;
+    return atol(size) * 1024 * 1024;
+  }
+  if (strcmp(size + (s - 2), "MB") == 0) {
+    size[s - 2] = 0;
+    return atol(size) * 1024;
+  }
+  if (strcmp(size + (s - 1), "B") == 0) {
+    size[s - 1] = 0;
+    return atol(size);
+  }
+  return 0;
+}
+
+void execute(char *filename) {
+  char *ar[1 << 10];
+  int i = 0;
+  char *token = strtok(Exec, " ");
+  while (token != NULL) {
+    ar[i] = (char *)malloc(sizeof(char) * strlen(token));
+    strcpy(ar[i], token);
+    token = strtok(NULL, " ");
+    i++;
+  }
+
+  ar[i] = filename;
+  i++;
+  ar[i] = NULL;
+  pid_t pid = fork();
+  if (pid == 0) // child process
+  {
+    execvp(ar[0], ar);
+    exit(0);
+  } else // parent process
+  {
+    wait(NULL);   // wait for the child to finish
+    kill(pid, 9); // then kill it.
+  }
+}
+
 time_t timetransform(char *timp) {
-  struct tm *tm;
-  if (strptime(time, "%a %m/%d/%Y %r", tm) == NULL) {
+  struct tm tm;
+  if (strptime(timp, "%d.%m.%Y %H:%M:%S", &tm) == NULL) {
     return 0;
   }
-  return mktime(tm);
+  return mktime(&tm);
 }
 
 int verificarenume(char *nume) {
@@ -63,17 +111,17 @@ int verificarePermisiuni(struct stat *fStat) {
     return 0;
   }
 
-  if (((fStat->st_mode & S_IRUSR) == (Permisiuni[0] == 'r' ? 1 : 0)) &&
-      ((fStat->st_mode & S_IWUSR) == (Permisiuni[1] == 'w' ? 1 : 0)) &&
-      ((fStat->st_mode & S_IXUSR) == (Permisiuni[2] == 'x' ? 1 : 0))
+  if (((fStat->st_mode & S_IRUSR) == (Permisiuni[0] == 'r' ? S_IRUSR : 0)) &&
+      ((fStat->st_mode & S_IWUSR) == (Permisiuni[1] == 'w' ? S_IWUSR : 0)) &&
+      ((fStat->st_mode & S_IXUSR) == (Permisiuni[2] == 'x' ? S_IXUSR : 0))
 
-      && ((fStat->st_mode & S_IRGRP) == (Permisiuni[3] == 'r' ? 1 : 0)) &&
-      ((fStat->st_mode & S_IWGRP) == (Permisiuni[4] == 'w' ? 1 : 0)) &&
-      ((fStat->st_mode & S_IXGRP) == (Permisiuni[5] == 'x' ? 1 : 0))
+      && ((fStat->st_mode & S_IRGRP) == (Permisiuni[3] == 'r' ? S_IRGRP : 0)) &&
+      ((fStat->st_mode & S_IWGRP) == (Permisiuni[4] == 'w' ? S_IWGRP : 0)) &&
+      ((fStat->st_mode & S_IXGRP) == (Permisiuni[5] == 'x' ? S_IXGRP : 0))
 
-      && ((fStat->st_mode & S_IROTH) == (Permisiuni[6] == 'r' ? 1 : 0)) &&
-      ((fStat->st_mode & S_IWOTH) == (Permisiuni[7] == 'w' ? 1 : 0)) &&
-      ((fStat->st_mode & S_IXOTH) == (Permisiuni[8] == 'x' ? 1 : 0)))
+      && ((fStat->st_mode & S_IROTH) == (Permisiuni[6] == 'r' ? S_IROTH : 0)) &&
+      ((fStat->st_mode & S_IWOTH) == (Permisiuni[7] == 'w' ? S_IWOTH : 0)) &&
+      ((fStat->st_mode & S_IXOTH) == (Permisiuni[8] == 'x' ? S_IXOTH : 0)))
     return 1;
   return 0;
 }
@@ -115,7 +163,7 @@ int verificare(struct stat *fStat) {
       return 0;
   }
 
-  if (Timestamp_Min != NULL) {
+  if (Timestamp_Max != NULL) {
     if (fStat->st_ctime > *Timestamp_Max)
       return 0;
   }
@@ -154,8 +202,12 @@ int info(char *path) {
     fprintf(stderr, "Error:stat error\n");
     return 0;
   }
-  if (verificare(&fstat) == 0)
+
+  if (verificare(&fStat) == 0)
     return 0;
+  if (Exec != NULL)
+    execute(path);
+  printf("<%s%s> ", s, path);
   fprintf(stdout, "<%ld> ", (long)fStat.st_blocks);        // Numar de inoduri:
   fprintf(stdout, "<%ld> ", (long)fStat.st_mode & S_IFMT); // file type :
   fprintf(stdout, "<%c%c%c%c%c%c%c%c%c%c> ",               // Permisiuni:
@@ -175,9 +227,11 @@ int info(char *path) {
   fprintf(stdout, "<%ld> ", fStat.st_nlink);     // Numar de legaturi:
   fprintf(stdout, "<%ld> ", (long)fStat.st_gid); // group ip :
   fprintf(stdout, "<%ld> ", (long)fStat.st_uid); // user id:
-  fprintf(stdout, "<%ld> \n", fStat.st_size);    // Dimensiune:
-  // fprintf(stdout, "<%ld> \n", fStat.st_birthtimespec); // timp creere
-
+  fprintf(stdout, "<%ld> ", fStat.st_size);      // Dimensiune:
+  char t[1 << 10];
+  strftime(t, 1 << 10, "%d.%m.%Y %H:%M:%S", localtime(&(fStat.st_ctime)));
+  fprintf(stdout, "<%s> \n", t); // timp creere
+  nr++;
   return 1;
 }
 
@@ -204,7 +258,6 @@ int find(const char *path, int depth) {
       if (verificarenume(entry->d_name) == 0)
         continue;
     }
-    printf("<%s%s> ", s, entry->d_name); // ia numele
     info(entry->d_name);
     if (entry->d_type == DT_DIR) // verifica daca ii folder
     {
@@ -240,17 +293,17 @@ int main(int argc, char *argv[]) {
         {"timestamp-max", required_argument, 0, 'T'},
         {"timestamp-mdata-min", required_argument, 0, 'm'},
         {"timestamp-mdata-max", required_argument, 0, 'M'},
+        {"exec", required_argument, 0, 'e'},
         {0, 0, 0, 0}};
     /* getopt_long stores the option index here. */
     int option_index = 0;
 
-    c = getopt_long(argc, argv, "a:p:P:i:D:d:u:g:h:H:t:T:m:M:", long_options,
+    c = getopt_long(argc, argv, "a:p:P:i:D:d:u:g:h:H:t:T:m:M:e:", long_options,
                     &option_index);
 
     /* Detect the end of the options. */
     if (c == -1)
       break;
-
     switch (c) {
     case 0:
       /* If this option set a flag, do nothing else now. */
@@ -266,7 +319,12 @@ int main(int argc, char *argv[]) {
       Adancime = atoi(optarg);
       break;
 
+    case 'e':
+      Exec = optarg;
+      break;
+
     case 'u':
+      User_Id = (int *)malloc(sizeof(int));
       *User_Id = atoi(optarg);
       break;
 
@@ -279,10 +337,12 @@ int main(int argc, char *argv[]) {
       break;
 
     case 'M':
+      Timestamp_Mdata_Max = (time_t *)malloc(sizeof(time_t));
       *Timestamp_Mdata_Max = timetransform(optarg);
       break;
 
     case 'm':
+      Timestamp_Mdata_Min = (time_t *)malloc(sizeof(time_t));
       *Timestamp_Mdata_Min = timetransform(optarg);
       break;
 
@@ -291,31 +351,38 @@ int main(int argc, char *argv[]) {
       break;
 
     case 'g':
-      Group_Id = atoi(optarg);
+      Group_Id = (int *)malloc(sizeof(int));
+      *Group_Id = atoi(optarg);
       break;
 
     case 'h':
+      Hardlink_Min = (int *)malloc(sizeof(int));
       *Hardlink_Min = atoi(optarg);
       break;
 
     case 'H':
+      Hardlink_Max = (int *)malloc(sizeof(int));
       *Hardlink_Max = atoi(optarg);
       break;
 
     case 't':
+      Timestamp_Min = (time_t *)malloc(sizeof(time_t));
       *Timestamp_Min = timetransform(optarg);
       break;
 
     case 'T':
+      Timestamp_Max = (time_t *)malloc(sizeof(time_t));
       *Timestamp_Max = timetransform(optarg);
       break;
 
     case 'd':
-      *Dim_Min = atol(optarg);
+      Dim_Min = (float *)malloc(sizeof(float));
+      *Dim_Min = compare(optarg);
       break;
 
     case 'D':
-      *Dim_Max = atol(optarg);
+      Dim_Max = (float *)malloc(sizeof(float));
+      *Dim_Max = compare(optarg);
       break;
 
     case '?':
@@ -327,6 +394,7 @@ int main(int argc, char *argv[]) {
     }
   }
   find(Path, Adancime);
+  printf("files nr: %d\n", nr);
 
   return 0;
 }
